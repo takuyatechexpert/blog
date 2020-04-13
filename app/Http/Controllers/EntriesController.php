c258422b022587d13ad998543ad0d9ff26d3e3ba
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-04-13 13:12:04 +0900

    entry-model-create

--- app/Http/Controllers/EntriesController.php
+++ app/Http/Controllers/EntriesController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Entry;
+
+// use Illuminate\Support\Facades\DB;
+// モデルを生成すれば記述は不要になる
 
 class EntriesController extends Controller
 {
     //indexアクション
     public function index() {
-        $entries = DB::select("select * from entries");
-        // クエリ文で取得している
-        var_dump($entries);
+        // $entries = DB::select("select * from entries");
+        // // クエリ文で取得している
+        $entries = Entry::all();
+        foreach ($entries as $entry){
+        var_dump($entry->title);}
     }
 }
